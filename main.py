25bdad45c7198d3f951640fef5b8660adbf1d644
garyhsieh15 <garyhsieh15@yahoo.com>
2017-11-19 01:03:23 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+input_para = [\
+        # index 0 --> b
+        10,\
+        # index  1 --> D
+        20,\
+        # index  2 --> d
+        30,\
+        # index  3 --> d_
+        40,\
+# index  4 --> fc (concrete strength)
+50,\
+# index  5 --> fy (steel strength)
+60,\
+# index  6 --> Ec (module)
+70,\
+# index  7 --> Es (module)
+80,\
+# index  8 --> phy
+90,\
+# index  9 --> a 
+20,\
+# index 10 --> As
+30,\
+# index 11 --> As_
+40,\
+# index 12 --> lo (As/bd)
+50,\
+# index 13 --> lo_
+60,\
+# index 14 --> Mu
+70,\
+# index 15 --> Mn
+80,\
+# index 16 --> con_strain
+90,\
+# index 17 --> stell_strain
+100,\
+# index 18 --> stell_strain_
+110]
+
+print("input_para[0]: ", input_para[0])
+print("input_para[1]: ", input_para[1])
+print("input_para[2]: ", input_para[2])
